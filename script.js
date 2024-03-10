d3b26d5eed2cfe996b0705545314594b79b24e04
Njabulo <nnshiwu@gmail.com>
2024-03-10 11:42:15 +0200

    Add an else block to handle the current song's position in the playlist.
    Within the else block, set the currentTime property of the audio object to the value stored in userData?.songCurrentTime.

--- script.js
+++ script.js
@@ -75,4 +75,7 @@ const playSong = (id) =>{
    if(userData?.currentSong == null || userData?.currentSong.id!== song.id){
       audio.currentTime = 0;
    }
+   else{
+      audio.currentTime = userData?.songCurrentTime;
+   }
 }
