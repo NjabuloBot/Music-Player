c7838d1fd0acc464fbad6748a296b868597e70e0
Njabulo <nnshiwu@gmail.com>
2024-03-12 12:29:19 +0200

    Use the setAttribute method on the playButton element to set an attribute named aria-label. For the value, use a ternary to set song?.title to Play {song.title} or Play if there's no song.title available.

--- script.js
+++ script.js
@@ -151,4 +151,5 @@ const setPlayerDisplay = () =>{
 
 const setPlayButtonAccessibleText = () =>{
    const song = userData?.currentSong || userData?.songs[0];
+   playButton.setAttribute("aria-label",song?.title ? `Play ${song.title}` : "Play");
 }
