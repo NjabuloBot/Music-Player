b8e1b1159790237388fb7f367c2dd8cf8d253459
Njabulo <nnshiwu@gmail.com>
2024-03-12 10:38:00 +0200

    Access the #player-song-title and #player-song-artist elements with the getElementById() method. Assign them to variables playingSong and songArtist respectively.

--- script.js
+++ script.js
@@ -140,5 +140,6 @@ const highlightCurrentSong = ()=>{
 }
 
 const setPlayerDisplay = () =>{
-   
+   const playingSong = document.getElementById("player-song-title");
+   const songArtist = document.getElementById("player-song-artist");
 }
