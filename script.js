c2d787972135dc4f21cde0c767a3e1c701c0fd2d
Njabulo <nnshiwu@gmail.com>
2024-03-12 10:22:13 +0200

    Within the callback function, use the removeAttribute() method to remove the aria-current attribute. This will remove the attribute for each of the songs.

--- script.js
+++ script.js
@@ -131,6 +131,6 @@ const highlightCurrentSong = ()=>{
    const playlistSongElements = document.querySelectorAll(".playlist-song");
    const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`);
    playlistSongElements.forEach((songEl) =>{
-      
+      songEl.removeAttribute("aria-current");
    });  
 }
