b8487590c487942e6013b0ae682575a91bb797ec
Njabulo <nnshiwu@gmail.com>
2024-03-12 10:19:53 +0200

    Use the forEach method on playlistSongElements. Pass in songEl as the parameter and use arrow syntax to add in an empty callback.

--- script.js
+++ script.js
@@ -130,4 +130,7 @@ const playPreviousSong= () =>{
 const highlightCurrentSong = ()=>{
    const playlistSongElements = document.querySelectorAll(".playlist-song");
    const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`);
+   playlistSongElements.forEach((songEl) =>{
+      
+   });  
 }
