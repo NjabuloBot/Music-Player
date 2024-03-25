0ad299bb2680be9c6e023ed1a09c0d4add2e84ac
Njabulo <nnshiwu@gmail.com>
2024-03-25 14:03:14 +0200

    Using const and arrow syntax to create an empty function called shuffle.
    This function is responsible for shuffling the songs in the playlist and performing necessary state management updates after the shuffling.

--- script.js
+++ script.js
@@ -154,3 +154,7 @@ const setPlayButtonAccessibleText = () =>{
    const song = userData?.currentSong || userData?.songs[0];
    playButton.setAttribute("aria-label",song?.title ? `Play ${song.title}` : "Play");
 }
+
+const shuffle = () =>{
+   
+}
